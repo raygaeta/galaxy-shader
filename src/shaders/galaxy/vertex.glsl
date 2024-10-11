507da4c17c283b0f940b911450f7d449ad5df4fd
raygaeta <raymondgaeta@gmail.com>
2024-10-11 16:47:14 -0700

    first commit

---
+++ src/shaders/galaxy/vertex.glsl
@@ -0,0 +1,29 @@
+uniform float uTime;
+uniform float uSize;
+attribute float aScale;
+attribute vec3 aRandomness;
+varying vec3 vColor;
+
+
+void main()
+{
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    //spin 
+    float angle = atan(modelPosition.x, modelPosition.z);
+    float distanceToCenter = length(modelPosition.xz);
+    float angleOffset = (1.0 / distanceToCenter) * uTime * 0.2;
+    angle += angleOffset;   
+    modelPosition.x = cos(angle) * distanceToCenter;
+    modelPosition.z = sin(angle) * distanceToCenter;
+
+    //add randomness
+    modelPosition.xyz += aRandomness.xyz;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectionPosition = projectionMatrix * viewPosition;
+    gl_Position = projectionPosition;
+    gl_PointSize = uSize * aScale;
+    gl_PointSize *= ( 1.0 / - viewPosition.z );
+    vColor = color;
+}
